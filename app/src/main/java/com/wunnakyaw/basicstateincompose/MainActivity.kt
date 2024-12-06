2b6a16c19f7e0a96832419c07c3c467d7be52516
Wunna Kyaw <wunnakyaw@globalwave.company>
2024-12-07 00:13:39 +0630

    Initial commit

---
+++ app/src/main/java/com/wunnakyaw/basicstateincompose/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.wunnakyaw.basicstateincompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.wunnakyaw.basicstatecodelab.WellnessScreen
+import com.wunnakyaw.basicstateincompose.ui.theme.BasicStateInComposeTheme
+
+class MainActivity : ComponentActivity() {
+    class MainActivity : ComponentActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContent {
+                BasicStateInComposeTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        WellnessScreen()
+                    }
+                }
+            }
+        }
+    }
+}
